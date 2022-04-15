39ac1caa24b58adb4ae65a56207e34d0e1f28041
OhYee <oyohyee@oyohyee.com>
2022-04-15 10:43:20 +0800

    fix(api): remove permission check when add blog

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -5,20 +5,26 @@ import DB from "@/utils/backend/db";
 
 export default wrapper<((_:{token?:string, id?:string, blog?: Blog})=>Promise<Result<any>>)>(
   async (args, req) => {
-    if (!(await DB.getUser({ token: shouldString(args.token) })).success) {
-      return { "success": false, "message": "You do not has permission" };
-    }
+  
 
     if (req.method === "PUT" && !!args.blog) {
       return await DB.addBlog({
         blog: args.blog as Blog 
       });
     } else if (req.method === 'POST' && !!args.id && !!args.blog) {
+      if (!(await DB.getUser({ token: shouldString(args.token) })).success) {
+        return { "success": false, "message": "You do not has permission" };
+      }
+      
       return await DB.updateBlog({
         id: shouldString(args.id),
         blog: args.blog as Blog,
       });
     } else if (req.method === 'DELETE' && args.id) { 
+      if (!(await DB.getUser({ token: shouldString(args.token) })).success) {
+        return { "success": false, "message": "You do not has permission" };
+      }
+      
       return await DB.deleteBlog({
         id: shouldString(args.id )
       });
